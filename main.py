8bb66b7ce30ce32424132d67f254fbd1b28dd989
jpschaumloeffel <katzetini@gmail.com>
2022-12-25 13:49:54 +0100

    make input format more robust by separating months

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+
 from icalendar import Calendar, Event
 
 year = 2023
@@ -6,39 +7,41 @@ bezirk = "3A1"
 
 local_tzinfo = datetime.now().astimezone().tzinfo
 
+# this is a really lazy way to input repeating events
+# - just list the days component of all events
+# - separate events in different months by commas
+# - make sure to have 12 months max
 calendar_data = {
-    'Restmüll': "4 18 1 15 1 15 29 13 26 10 24 7 21 5 19 2 16 30 13 27 11 25 8 22 6 20",
-    'Gelber Sack': "4 1 1 29 26 24 21 19 16 13 11 8 6",
-    'Papiertonne': "25 23 22 19 17 14 12 9 6 5 2 29 28",
-    'Biotonne': "11 25 8 23 8 22 5 19 4 17 1 14 28 12 26 9 23 6 20 5 18 2 15 29 13 28",
+    'Restmüll': "4 18, 1 15, 1 15 29, 13 26, 10 24, 7 21, 5 19, 2 16 30, 13 27, 11 25, 8 22, 6 20",
+    'Gelber Sack': "4, 1, 1 29, 26, 24, 21, 19, 16, 13, 11, 8, 6",
+    'Papiertonne': "25, 23, 22, 19, 17, 14, 12, 9, 6, 5, 2 29, 28",
+    'Biotonne': "11 25, 8 23, 8 22, 5 19, 4 17, 1 14 28, 12 26, 9 23, 6 20, 5 18, 2 15 29, 13 28",
 }
 
 
 def generate_ics():
     cal = Calendar()
 
-    for calname, days in calendar_data.items():
-        parsed_days = [int(x) for x in days.split(" ")]
+    for calname, caldata in calendar_data.items():
+        monthly_days = [x.strip() for x in caldata.split(",")]
 
-        dates = []
+        if len(monthly_days) != 12:
+            print(f"warning: data for {calname} contains events in {len(monthly_days)} months, probably wrong")
 
-        month = 1
-        last_day = None
-        for day in parsed_days:
-            if last_day is not None and last_day >= day:
-                month += 1
-            dates.append(datetime(year, month, day, tzinfo=local_tzinfo).date())
-            last_day = day
+        for month, month_days in enumerate(monthly_days):
+            for day in [int(x) for x in month_days.split(" ")]:
+                if (month + 1) < 1 or (month + 1) > 12:
+                    pass
+                d = datetime(year, month + 1, day, tzinfo=local_tzinfo).date()
 
-        for d in dates:
-            event = Event()
-            event[
-                'uid'] = f'muellkalender2023-{calname.encode("ascii", errors="ignore").decode()}-{d.year}{d.month:02}{d.day:02}'
-            event.add('summary', calname)
-            event.add('description', f"Bezirk {bezirk}")
-            event.add('dtstart', d)
-            event.add('dtend', d + timedelta(days=1))
-            cal.add_component(event)
+                event = Event()
+                ascii_calname = calname.encode("ascii", errors="ignore").decode()
+                event['uid'] = f'muellkalender2023-{ascii_calname}-{d.year}{d.month:02}{d.day:02}'
+                event.add('summary', calname)
+                event.add('description', f"Bezirk {bezirk}")
+                event.add('dtstart', d)
+                event.add('dtend', d + timedelta(days=1))
+                cal.add_component(event)
 
     return cal.to_ical()
 
